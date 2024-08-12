6694528c0756209f33f7c27d3b020e9d1c48f031
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-12 12:27:51 -0500

    add shortcut skeleton

---
+++ portfoliowebsiteupgraded.client/src/components/Shortcut/Shortcut.tsx
@@ -0,0 +1,10 @@
+function Shortcut(name: string, image_uri: string) {
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{image_uri}</p>
+        </div>
+    );
+}
+
+export default Shortcut;
