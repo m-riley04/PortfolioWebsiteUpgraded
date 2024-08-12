c0a0e3003be9a3d1bd7efb89a3ce84a6c074c8f7
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-11 20:32:21 -0500

    create project controller

---
+++ PortfolioWebsiteUpgraded.Server/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioWebsiteUpgraded.Server.Data_Accessors;
+using PortfolioWebsiteUpgraded.Server.Records;
+
+namespace PortfolioWebsiteUpgraded.Server.Controllers
+{
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectController
+    {
+        public ProjectDataAccessor dbAccessor = new();
+
+        [HttpGet]
+        [Route("all")]
+        public JsonResult GetAll()
+        {
+            return new JsonResult(dbAccessor.GetProjects());
+        }
+
+        [HttpPut]
+        [Route("")]
+        public JsonResult Put([FromBody] Project project)
+        {
+            dbAccessor.InsertProject(project);
+            return new JsonResult("Success!");
+        }
+    }
+}
