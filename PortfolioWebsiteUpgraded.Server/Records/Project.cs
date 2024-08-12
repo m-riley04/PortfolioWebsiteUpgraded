0326148a7bde965b1aae7a223f9b391ee052f77f
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-11 20:32:40 -0500

    create project record

---
+++ PortfolioWebsiteUpgraded.Server/Records/Project.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace PortfolioWebsiteUpgraded.Server.Records
+{
+    public record Project(int ProjectId, string Name, string? Description, int AuthorId, string? Collaborators, string? Images, string? RepositoryUri, string? ReleaseUri, string? ReadmeUri, string? ExtraData)
+    {
+
+        public static Project FromRow(DataRow row)
+        {
+            return new Project(
+                row.Field<int>("project_id")
+                , row.Field<string>("name") ?? ""
+                , row.Field<string?>("description")
+                , row.Field<int>("author_id")
+                , row.Field<string?>("collaborators")
+                , row.Field<string?>("images")
+                , row.Field<string?>("repository_uri")
+                , row.Field<string?>("release_uri")
+                , row.Field<string?>("readme_uri")
+                , row.Field<string?>("extra_data"));
+        }
+    }
+}
