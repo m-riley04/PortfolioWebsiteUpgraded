f2178f22c346344c1699c1675953396d120cf8f7
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-13 16:16:22 -0500

    add WindowEnum instead of an iterative id

--- portfoliowebsiteupgraded.client/src/App.tsx
+++ portfoliowebsiteupgraded.client/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Project from './pages/Projects.tsx';
+import Project from './windows/Projects/Projects.tsx';
 import Desktop from './pages/Desktop/Desktop.tsx';
 import IShortcut from './interfaces/IShortcut.ts';
+import AboutMe from './windows/AboutMe/AboutMe.tsx';
+import WindowEnum from './enums/WindowEnum.ts';
 
 const shortcuts: IShortcut[] = [
-    { name: "Projects", image_uri: "vite.svg", window: { id: 0, title: "Projects", element: <Project></Project>, x: 0, y: 0 } },
-    { name: "About Me", image_uri: "vite.svg"},
-    { name: "Recycle Bin", image_uri: "vite.svg"}
+    { name: "Projects", image_uri: "directory_open.png", window: { id: WindowEnum.PROJECTS, element: <Project/>, x: 0, y: 0 } },
+    { name: "About Me", image_uri: "users.png", window: { id: WindowEnum.ABOUT_ME, element: <AboutMe/>, x: 0, y: 0 } },
+    { name: "Recycle Bin", image_uri: "recycle_bin_empty.png"}
 ]
 
 function App() {
@@ -15,8 +17,6 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Desktop shortcuts={shortcuts}/>}></Route>
-                <Route path="projects" element={<Project/>}></Route>
-                <Route path="about-me" element={<></>}></Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ portfoliowebsiteupgraded.client/src/enums/WindowEnum.ts
@@ -0,0 +1,9 @@
+/** 
+ * The identity of the window
+ */
+enum WindowEnum {
+    PROJECTS,
+    ABOUT_ME
+}
+
+export default WindowEnum;
