8a4c63aad935e61e1729a12f56295797546deb65
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-13 16:15:47 -0500

    flesh out window controls

--- portfoliowebsiteupgraded.client/src/components/Window/WindowControls/WindowControls.tsx
+++ portfoliowebsiteupgraded.client/src/components/Window/WindowControls/WindowControls.tsx
@@ -2,6 +2,7 @@ import { MouseEventHandler } from "react";
 
 interface IWindowControls {
     title: string;
+    icon_uri: string;
     hasClose?: boolean;
     hasMinimize?: boolean;
     hasMaximize?: boolean;
@@ -14,11 +15,24 @@ interface IWindowControls {
 function WindowControls(props: IWindowControls) {
 
     return (
-        <div className={`window-controls ${props.className ?? ""}`}>
-            <p>{props.title}</p>
-            {props.hasMinimize ? <button className="minimize-button" onClick={props.onMinimize}>Minimize</button> : <></> }
-            {props.hasMaximize ? <button className="maximize-button" onClick={props.onMaximize}>Maximize</button> : <></> }
-            {props.hasClose ? <button className="close-button" onClick={props.onClose}>Close</button> : <></>}
+        <div>
+            <div className={`window-controls ${props.className ?? ""}`}>
+                <div className="title-container">
+                    <img src={props.icon_uri}></img>
+                    <p>{props.title}</p>
+                </div>
+                <div className="button-container">
+                    {props.hasMinimize ? <button className="minimize-button" onClick={props.onMinimize}>-</button> : <></>}
+                    {props.hasMaximize ? <button className="maximize-button" onClick={props.onMaximize}>O</button> : <></>}
+                    {props.hasClose ? <button className="close-button" onClick={props.onClose}>X</button> : <></>}
+                </div>
+            </div>
+            <div className="dropdowns">
+                <button>File</button>
+                <button>Edit</button>
+                <button>View</button>
+                <button>Help</button>
+            </div>
         </div>
     );
 }
