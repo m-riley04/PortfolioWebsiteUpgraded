fcb2dabac5f506c8807d79a072ff5eb16ddeb8e8
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-13 16:14:14 -0500

    add icon_uri prop

--- portfoliowebsiteupgraded.client/src/interfaces/IWindow.ts
+++ portfoliowebsiteupgraded.client/src/interfaces/IWindow.ts
@@ -1,10 +1,11 @@
 import { ReactElement } from "react";
 
 export default interface IWindow {
+    id: number;
     title: string;
+    icon_uri: string;
     element: ReactElement;
     x?: number;
     y?: number;
     onClose?: (i: number) => void;
-    id: number;
 }
