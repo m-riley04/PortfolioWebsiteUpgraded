4e8b2eb2b2c2d381376740de9959837236175815
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-13 12:40:26 -0500

    create window management system

--- portfoliowebsiteupgraded.client/src/pages/Desktop/Desktop.tsx
+++ portfoliowebsiteupgraded.client/src/pages/Desktop/Desktop.tsx
@@ -1,18 +1,65 @@
+import { useEffect, useState } from "react";
 import Shortcut from "../../components/Shortcut/Shortcut";
+import IShortcut from "../../interfaces/IShortcut";
 import "../Desktop/Desktop.scss";
+import IWindow from "../../interfaces/IWindow";
+import Window from "../../components/Window/Window";
 
-interface ShortcutData {
-    name: string;
-    image_uri: string;
-}
+let windowIdCounter = 0;
+const X_PADDING = 50;
 
-function Desktop(props: { shortcuts?: ShortcutData[] }) {
+function Desktop(props: { shortcuts?: IShortcut[] }) {
+    const [windows, setWindows] = useState<IWindow[]>([]);
+
+    useEffect(() => {
+
+    }, [windows])
+
+    function getUniqueWindowId(): number {
+        return ++windowIdCounter;
+    }
+
+    function addWindow(window?: IWindow) {
+        if (window) {
+            const newWindow = { ...window, id: getUniqueWindowId() };
+            setWindows([...windows, newWindow]);
+            console.log("Window added");
+        }
+    }
+
+    function removeWindowByKey(id: number) {
+        setWindows(windows.filter((window) => window.id !== id));
+        console.log(`Window '${id}' removed`);
+    }
+
     return (
         <div className="desktop">
-            {
-                props.shortcuts?.map((shortcut, i) =>
-                    <Shortcut key={i} name={shortcut.name} image_uri={shortcut.image_uri}/>
-                ) ?? <Shortcut name="Default" image_uri="cat.jpg"/>
+            { // Create shortcuts
+                props.shortcuts?.map((shortcut, i) => {
+                    const window: IWindow | undefined = shortcut.window;
+
+                    return <Shortcut key={i}
+                        name={shortcut.name}
+                        image_uri={shortcut.image_uri}
+                        window={window}
+                        x={X_PADDING * i}
+                        y={0}
+                        onOpen={() => addWindow(window)}/>
+                }
+                ) ?? <Shortcut name="Default" image_uri="vite.svg" x={0} y={0} />
+            }
+
+            { // Create windows
+                windows.map((window) => {
+                    return <Window
+                        key={window.id}
+                        id={window.id}
+                        title={window.title}
+                        element={window.element}
+                        onClose={() => removeWindowByKey(window.id)}
+                        x={window.x}
+                        y={window.y}/>;
+                })
             }
         </div>
     );
