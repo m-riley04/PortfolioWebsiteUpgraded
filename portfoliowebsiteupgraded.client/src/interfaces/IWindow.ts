e5ff2a96446b3f5bcd6613b8a6fa1b17bb2cdf4b
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-14 12:03:03 -0500

    add width, height, and type

--- portfoliowebsiteupgraded.client/src/interfaces/IWindow.ts
+++ portfoliowebsiteupgraded.client/src/interfaces/IWindow.ts
@@ -1,11 +1,15 @@
 import { ReactElement } from "react";
+import WindowTypeEnum from "../enums/WindowTypeEnum";
 
 export default interface IWindow {
     id: number;
-    title: string;
-    icon_uri: string;
+    title?: string;
+    icon_uri?: string;
     element: ReactElement;
+    width?: number;
+    height?: number;
     x?: number;
     y?: number;
     onClose?: (i: number) => void;
+    type?: WindowTypeEnum;
 }
