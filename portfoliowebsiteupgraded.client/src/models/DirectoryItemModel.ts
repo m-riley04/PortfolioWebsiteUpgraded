2f3a65537f96065ca2dbddfa4f5e881247ef9563
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-08-14 12:03:16 -0500

    add onClick and icon_uri

--- portfoliowebsiteupgraded.client/src/models/DirectoryItemModel.ts
+++ portfoliowebsiteupgraded.client/src/models/DirectoryItemModel.ts
@@ -2,7 +2,9 @@ import FileTypeEnum from "../enums/FileTypeEnum";
 
 export default interface DirectoryItemModel {
     name: string;
+    icon_uri?: string;
     description: string;
     dateModified: Date;
     type: FileTypeEnum;
+    onClick?: () => void;
 }
