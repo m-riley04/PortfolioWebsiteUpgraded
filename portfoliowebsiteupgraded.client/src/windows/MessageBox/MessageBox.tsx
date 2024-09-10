d602e0b444f6b3220f125c74c143758f840e5cb6
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-09-10 18:52:05 -0500

    fix message box styling

--- portfoliowebsiteupgraded.client/src/windows/MessageBox/MessageBox.tsx
+++ portfoliowebsiteupgraded.client/src/windows/MessageBox/MessageBox.tsx
@@ -1,10 +1,14 @@
 import { FunctionComponent } from "react";
 import MessageBoxIconEnum from "../../enums/MessageBoxIconEnum";
 import "../MessageBox/MessageBox.scss";
+import ButtonOption from "../../models/ButtonOption";
+import { useWindowManager } from "../../contexts/WindowContext/WindowContext";
 
 interface IMessageBox {
+    parentWindowTitle: string,
     icon: MessageBoxIconEnum;
     text: string;
+    buttons?: ButtonOption[]
 }
 
 function getMessageBoxIcon(icon: MessageBoxIconEnum): string {
@@ -23,13 +27,33 @@ function getMessageBoxIcon(icon: MessageBoxIconEnum): string {
 }
 
 const MessageBox: FunctionComponent<IMessageBox> = ({
+    parentWindowTitle,
     text = "Lorem ipsum",
-    icon = MessageBoxIconEnum.INFORMATION
+    icon = MessageBoxIconEnum.INFORMATION,
+    buttons = []
 }) => {
+    const { getWindowIdByTitle, removeWindowByKey } = useWindowManager();
+
+    // Initialize default buttons
+    if (buttons.length < 1) {
+        const windowId: number = getWindowIdByTitle(parentWindowTitle);
+        buttons = [{ title: "Ok", onClick: () => removeWindowByKey(windowId)}]
+    }
+
+    const buttonsContainer = (buttons?.length > 0) ? (
+        <div className="dialog-button-container">
+            {buttons.map((option, i) => <button key={i} onClick={option.onClick}><u>{option.title.charAt(0)}</u>{option.title.substring(1)}</button>)}
+        </div>
+    ) : <></>;
+
     return (
         <div className="message-box">
-            <img src={getMessageBoxIcon(icon)} />
-            <p>{text}</p>
+            <div className="message-container">
+                <img src={getMessageBoxIcon(icon)} />
+                <p>{text}</p>
+            </div>
+            
+            {buttonsContainer}
         </div>
     );
 }
